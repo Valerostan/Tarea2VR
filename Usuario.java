7dd7bf733691fd4498e2809e05a796c72f440dfb
Vale <valerostan@hotmail.com>
2020-03-31 18:17:22 -0300

    atributos

---
+++ Usuario.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea2.pkg1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Usuario {
+    private String nombre;
+    private int edad;
+    private String contraseña;
+    private int cedula;
+    private int cantHijos;
+    private int saldo;
+    
+   
+
+    public Usuario() {
+        this.saldo = 0;
+    }
+
+    
+}
+
